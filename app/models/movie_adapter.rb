f396d9bd18abc473a74f267dc9ea99feb6ace768
Dawson Graham <hdawsong@gmail.com>
2017-05-31 14:55:24 -0700

    Finalize search method for MovieAdapter. API calls working properly

--- app/models/movie_adapter.rb
+++ app/models/movie_adapter.rb
@@ -2,6 +2,6 @@ class MovieAdapter
   def search(movie_title)
     movie = movie_title
 
-    request = HTTParty.get("http://www.omdbapi.com/?t=#{movie}&apikey=c173dc09")
+    request = HTTParty.get("http://www.omdbapi.com/?t=#{movie}&apikey=c173dc09").parsed_response
   end
 end
