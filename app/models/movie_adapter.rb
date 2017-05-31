0368437e13a9ff72d07fe21bd79bee07f5907c82
Dawson Graham <hdawsong@gmail.com>
2017-05-31 15:34:58 -0700

    Refactor MovieAdapter to be more DRY

--- app/models/movie_adapter.rb
+++ app/models/movie_adapter.rb
@@ -1,7 +1,6 @@
 class MovieAdapter
   def search(movie_title)
-    movie = movie_title
     key = ENV['API_AUTH_KEY']
-    request = HTTParty.get("http://www.omdbapi.com/?t=#{movie}&apikey=#{key}").parsed_response
+    request = HTTParty.get("http://www.omdbapi.com/?t=#{movie_title}&apikey=#{key}").parsed_response
   end
 end
