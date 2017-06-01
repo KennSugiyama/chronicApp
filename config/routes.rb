74d8a471196fc5e29bc08c0deb95c24050164e5b
brn-gthbrg <eleanor@gmail.com>
2017-06-01 14:59:07 -0700

    avoid merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,7 @@ Rails.application.routes.draw do
   get 'movies/index'
 
   devise_for :users
-<<<<<<< HEAD
-
-  # resources :users
-=======
   resources :users
->>>>>>> e2382fba3272dee805d2e5ea41ce8298dda5eda8
 
   resources :movies
 
