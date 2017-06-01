1ab75e48b917066de6ef92ac141e4dd4b82b11e3
Kenn Sugiyama & Mina Melosh <pair+kennsugiyama.minamelosh@devbootcamp.com>
2017-05-31 20:46:44 -0700

    create views and partials for editing reviews and comments.  Create edit and destroy methods for comments and reviews in the controllers.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,11 @@
 class CommentsController < ApplicationController
+  # def index
+  #   @comments = Comment.all
+  # end
 
-  def index
-    @comments = Comment.all
-  end
-
-  def show
-    @comment = Comment.find(params[:id])
-  end
+  # def show
+  #   @comment = Comment.find(params[:id])
+  # end
 
   def new
     @comment = Comment.new
@@ -20,6 +19,23 @@ class CommentsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to @comment
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to comments_path
   end
 
   private
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,17 @@
 class ReviewsController < ApplicationController
+  # def index
+  #   @reviews = Review.all
+  # end
 
-  def index
-    @reviews = Review.all
-  end
-
-  def show
-    @review = Review.find(params[:id])
-  end
+  # def show
+  #   @review = Review.find(params[:id])
+  # end
 
   def new
     @review = Review.new
   end
 
-    def create
+  def create
     @review = Review.new(review_params)
 
     if @review.save
@@ -20,6 +19,23 @@ class ReviewsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @review = Review.find(params[:id])
+
+    if @review.update(review_params)
+      redirect_to @review
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+
+    redirect_to reviews_path
   end
 
   private
