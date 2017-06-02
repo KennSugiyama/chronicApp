658779793d0d53719db64bd5f13fba5120ca1853
brn-gthbrg <eleanor@gmail.com>
2017-06-01 17:27:42 -0700

    update routes to remove extra users resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Override devise controller to use our custom RegistrationsController
   devise_for :users, :controllers => {registrations: 'registrations'}
-  resources :users
+  # resources :users
 
   resources :movies
 
