3d6c718fa5ce27453145ba0725b2c4864672b7f0
Dawson Graham <email": null>
2017-06-01 18:58:47 -0700

    Fix user registration with username and proper showing in movie show page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
-    
+  before_action :configure_permitted_parameters
+
  def create
+  p params
+  p '*' *50
       super
       #Send email to the user when sign up
       UserMailer.welcome_email(@user).deliver
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:user, keys: [:username])
   end
 
 end
