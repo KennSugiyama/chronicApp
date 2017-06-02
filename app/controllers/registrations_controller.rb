faa95e643e646f0ababe773759fd87f664b33ab5
brn-gthbrg <eleanor@gmail.com>
2017-06-01 17:26:05 -0700

    clean up unnecessary methods

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,22 +6,4 @@ class RegistrationsController < Devise::RegistrationsController
       UserMailer.welcome_email(@user).deliver
   end
 
- private
-
- def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar)
-  end
-
- def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
-  end
-
-    # def after_sign_in_path_for(resource_or_scope)
-    #     current_user
-    # end
-
-#  def after_update_path_for(resource)
- #    current_user
- #  end
-
 end
