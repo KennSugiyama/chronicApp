3299e2e4a3519ef5a836f900e8d16fbb0be89e69
Dawson Graham <hdawsong@gmail.com>
2017-06-02 18:12:00 -0700

    Final MVP

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,20 +10,37 @@ class CommentsController < ApplicationController
   def new
     @review = Review.find(params[:review_id])
     @comment = Comment.new
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def create
     @review = Review.find(params[:review_id])
+    @movie = @review.movie
     @comment = @review.comments.new(comment_params)
     @comment.user_id = current_user.id
-    @comment.save
-    redirect_to movie_path(@review.movie)
+
+    respond_to do |format|
+      if @comment.save
+        format.html {redirect_to movie_path(@movie)}
+        format.js {redirect_to movie_path(@movie)}
+      else
+        format.html {redirect_to new_movie_comment_path(@review)}
+      end
+    end
   end
 
   def edit
     @review = Review.find(params[:review_id])
     @comment = Comment.find(params[:id])
-    render 'edit'
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def update
@@ -31,10 +48,13 @@ class CommentsController < ApplicationController
     @movie = @review.movie.id
     @comment = Comment.find(params[:id])
 
-    if @comment.update(comment_params)
-      redirect_to movie_path(@movie)
-    else
-      render 'edit'
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html {redirect_to movie_path(@movie)}
+        format.js {redirect_to movie_path(@movie)}
+      else
+        format.html {redirect_to edit_review_comment_path(@movie, comment)}
+      end
     end
   end
 
